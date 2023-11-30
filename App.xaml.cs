ce005b108479b9aded5762e5358541df4fa090ab
MariaStrimbei <maria.strimbei@gmail.com>
2023-11-30 20:14:32 +0200

    lab8

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Strimbei_Maria_Alexandra_Lab7
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
